573e6a596ce236939644e4b4730553f611defc52
medwf <med.wf95@gmail.com>
2025-05-05 11:12:11 +0000

    fix files structures

--- src/solutions/str-utils.ts
+++
@@ -1,9 +0,0 @@
-declare module 'str-utils' {
-  // export const ...
-  // export function ...
-  function strReverse(value: string): string;
-  function strToLower(value: string): string;
-  function strToUpper(value: string): string;
-  function strRandomize(value: string): string;
-  function strInvertCase(value: string): string;
-}
